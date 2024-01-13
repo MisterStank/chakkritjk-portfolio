5d3c5f6fb678ef8e49011517adbdf39ee91fb828
MisterStank <chakkritchongkraichak@gmail.com>
2024-01-14 00:01:40 +0700

    projects section

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,11 +1,17 @@
+"use client"
 import React from 'react'
 import SectionHeading from './section-heading'
 import { projectsData } from '@/lib/data'
-import Image from 'next/image';
+import Project from './project'
+import { motion } from 'framer-motion'
 
 export default function Projects() {
   return (
-    <section>
+    <motion.section className='mb-28 max-w-[45rem]
+    text-center leading-8 sm:mb-40'
+    initial={{opacity:0, y:100}}
+    animate={{opacity:1, y:100}}
+    transition={{delay:0.190}}>
         <SectionHeading>My projects</SectionHeading>
         <div>
             {
@@ -16,38 +22,6 @@ export default function Projects() {
                 ))
             }
         </div>
-    </section>
+    </motion.section>
   )
-}
-
-type ProjectProps = (typeof projectsData)[number];
-
-function Project({
-    title,
-    description,
-    tags,
-    imageUrl
-}: ProjectProps){
-    return <section className='bg-gray-100 max-w-[42rem]
-    border border-black/5 overflow-hidden sm:pr-8 relative
-    sm:h-[20rem] mb-3 sm:mb-8 last:mb-0'>
-        <div className='pt-4 pb-8 px-5 sm:pl-10 sm:pr-2 sm:pt-10
-        sm:max-w-[50%] flex flex-col h-full'>
-            <h3 className='text-2xl font-semibold'>{title}</h3>
-            <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
-            <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
-            {tags.map((tag, index) => (
-              <li
-                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
-                key={index}
-              >
-                {tag}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <Image src={imageUrl} alt="Project I worked on" quality={95}
-        className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg
-        shadow-2xl'/>
-    </section>
 }
